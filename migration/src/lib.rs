9a21a88fea1e3c4c6f9925f13d8455966d1b0581
rh-jfuller <jfuller@redhat.com>
2026-09-17 12:24:28 +0200

    fix: keep backport migration history branch-specific

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -66,17 +66,6 @@ mod m0002210_sbom_node_name_index;
 mod m0002220_drop_qualified_purl_gist_indexes;
 mod m0002230_sle_license_id_index;
 mod m0002240_product_version_sbom_index;
-mod m0002250_create_cpe_status;
-mod m0002260_cpe_part_vendor_product_index;
-mod m0002270_fix_vulnerability_base_score_type;
-mod m0002280_backfill_sbom_suppliers;
-mod m0002290_create_exploit_intelligence_job;
-mod m0002300_create_exploit;
-mod m0002310_create_change_log;
-mod m0002320_fix_unbounded_version_matches;
-mod m0002330_fix_rpmver_cmp;
-mod m0002340_backfill_rpm_epoch;
-mod m0002350_drop_purl_status_gist_index;
 mod m0002360_pythonver_cmp_parallel_restricted;
 
 pub trait MigratorExt: Send {
@@ -159,17 +148,6 @@ impl MigratorExt for Migrator {
             .normal(m0002220_drop_qualified_purl_gist_indexes::Migration)
             .normal(m0002230_sle_license_id_index::Migration)
             .normal(m0002240_product_version_sbom_index::Migration)
-            .normal(m0002250_create_cpe_status::Migration)
-            .normal(m0002260_cpe_part_vendor_product_index::Migration)
-            .normal(m0002270_fix_vulnerability_base_score_type::Migration)
-            .data(m0002280_backfill_sbom_suppliers::Migration)
-            .normal(m0002290_create_exploit_intelligence_job::Migration)
-            .normal(m0002300_create_exploit::Migration)
-            .normal(m0002310_create_change_log::Migration)
-            .normal(m0002320_fix_unbounded_version_matches::Migration)
-            .normal(m0002330_fix_rpmver_cmp::Migration)
-            .normal(m0002340_backfill_rpm_epoch::Migration)
-            .normal(m0002350_drop_purl_status_gist_index::Migration)
             .normal(m0002360_pythonver_cmp_parallel_restricted::Migration)
     }
 }
